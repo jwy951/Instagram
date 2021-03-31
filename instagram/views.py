81b25a92af26f72fc1a1904c7e79020237d7ac70
Lewis Njagi <lewismaybach@gmail.com>
2021-03-31 17:22:11 +0300

    add profile view function

--- instagram/views.py
+++ instagram/views.py
@@ -29,25 +29,25 @@ def post(request):
         form = UploadForm()
     return render(request,'post_image.html', {"form":form})
 
-# def profile(request, username):
-#     images = request.user.profile.images.all()
-#     print(images)
-#     if request.method == 'POST':
-#         user_form = UpdateUserForm(request.POST, instance=request.user)
-#         prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
-#         if user_form.is_valid() and prof_form.is_valid():
-#             user_form.save()
-#             prof_form.save()
-#             return HttpResponseRedirect(request.path_info)
-#     else:
-#         user_form = UpdateUserForm(instance=request.user)
-#         prof_form = UpdateUserProfileForm(instance=request.user.profile)
-#     params = {
-#         'user_form': user_form,
-#         'prof_form': prof_form,
-#         'images': images,
-#     }
-#     return render(request, 'profile.html', params)
+def profile(request, username):
+    images = request.user.profile.images.all()
+    print(images)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form,
+        'images': images,
+    }
+    return render(request, 'profile.html', params)
 
 # def update_profile(request):
 #     if request.method == 'POST':

5a238d1285f1325ccc66cd397620f627cc7100b1
Lewis Njagi <lewismaybach@gmail.com>
2021-04-01 20:38:53 +0300

    add account path

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django_registration.backends.one_step.views import RegistrationView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('instagram.urls')),
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/'),
+        name='django_registration_register'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+
 ]

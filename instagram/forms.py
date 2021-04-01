01d724698a3b8a627834ab930f7213346a8f87c7
Lewis Njagi <lewismaybach@gmail.com>
2021-04-01 20:34:43 +0300

    add updateUser form

--- instagram/forms.py
+++ instagram/forms.py
@@ -14,12 +14,12 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ('photo','name','bio')
 
-# class UpdateUserForm(forms.ModelForm):
-#     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email')
+    class Meta:
+        model = User
+        fields = ('username', 'email')
 
 # class UpdateUserProfileForm(forms.ModelForm):
 #     class Meta:

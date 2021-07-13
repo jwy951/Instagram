d07cc5a5421789e6636e82da5475a866e3c74223
Santa claus <santa@nortpole.com>
2021-07-13 10:39:31 +0300

    add templates

--- instagram/email.py
+++ instagram/email.py
@@ -3,8 +3,8 @@ from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
-    subject = 'Welcome to the MoringaTribune NewsLetter'
-    sender = 'lewismaybach@gmail.com'
+    subject = 'Welcome to the instagram'
+    sender = 'johnstoneshylock.com'
 
     #passing in the context vairables
     text_content = render_to_string('email/newsemail.txt',{"name": name})
